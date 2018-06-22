a6a2019850c1c387ae19010694d748ee0a4456f2
Anna Riehle <riehlea@gmail.com>
2018-06-21 20:45:44 -0400

    Fix hover, click, and zoom; add URLs and basic DC info

--- auxiliary/dc-maps/logic.js
+++ auxiliary/dc-maps/logic.js
@@ -3,6 +3,8 @@ quadrantURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/d
 wardURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/ward-2012.geojson"
 ancURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/advisory-neighborhood-commission-2013.geojson"
 smdURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/single-member-district-2013.geojson"
+neighborhoodsURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/neighborhood-clusters.geojson"
+zipcodesURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/zip-codes.geojson"
 
 var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
     "access_token=pk.eyJ1IjoicmllaGxlYSIsImEiOiJjamlhdWlzcnkxMndiM3FsbWl1aXE0MXJtIn0.g7oyFuzbGAh1O0SXpGI8nw");
@@ -27,7 +29,14 @@ function chooseColor(quadrant) {
 }
 
 d3.json(boundaryURL, function(boundaryData) {
-  var boundary = L.geoJSON(boundaryData)
+  var boundary = L.geoJSON(boundaryData, {
+        onEachFeature: function (feature, layer) {
+    var marker1 = layer.bindPopup('<h1>Washington, DC</h1><h3>Mayor: Muriel Bowser</h3><h3>Population: 693,972</h3><h3>Area: '+feature.properties.AREAMILES+' Square Miles</h3><h3><a href='+feature.properties.WEB_URL+' target="_blank">Discover DC</a></h3>');
+    marker1.on('mouseover', function (event) {
+  this.openPopup();
+});
+  }
+})
 
   var myMap = L.map("map", {
     center: [
@@ -43,6 +52,13 @@ d3.json(quadrantURL, function(quadrantData) {
     layer.on('click', function (event) {
 myMap.fitBounds(event.target.getBounds());
 });
+    var marker1 = layer.bindPopup('<h3>'+feature.properties.QUADRANT+'</h3>');
+    marker1.on('mouseover', function (event) {
+      this.openPopup()
+    })
+    marker1.on('mouseout', function (event) {
+      this.closePopup()
+    })
   },
     style: function(feature) {
       return {
@@ -55,8 +71,14 @@ myMap.fitBounds(event.target.getBounds());
 })
 d3.json(wardURL, function(wardData) {
   var ward = L.geoJSON(wardData, {
+            style: function(feature) {
+      return {
+        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        color: "red"
+      };
+    },
     onEachFeature: function (feature, layer) {
-    var marker1 = layer.bindPopup('<h3> Ward'+feature.properties.WARD_ID+'</h3>');
+    var marker1 = layer.bindPopup('<h3> Ward '+feature.properties.WARD_ID+'</h3><h4><a href="https://planning.dc.gov/page/about-ward-'+feature.properties.WARD_ID+'" target="_blank">About Ward '+feature.properties.WARD_ID+'</a>');
     marker1.on('mouseover', function (event) {
   this.openPopup();
   layer = event.target
@@ -79,6 +101,12 @@ marker1.on('mouseout', function (event) {
 
 d3.json(smdURL, function(smdData) {
   var smd = L.geoJSON(smdData, {
+        style: function(feature) {
+      return {
+        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        color: "red"
+      };
+    },
   onEachFeature: function (feature, layer) {
     var marker2 = layer.bindPopup('<h3>ANC'+feature.properties.SMD_ID+'</h3>');
     marker2.on('mouseover', function (event) {
@@ -103,8 +131,14 @@ marker2.on('mouseout', function (event) {
 
 d3.json(ancURL, function(ancData) {
   var anc = L.geoJSON(ancData, {
+    style: function(feature) {
+      return {
+        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        color: "red"
+      };
+    },
   onEachFeature: function (feature, layer) {
-    var marker3 = layer.bindPopup('<h3>'+feature.properties.NAME+'</h3>');
+    var marker3 = layer.bindPopup('<h3>'+feature.properties.NAME+'</h3><h4><a href="http://www.anc'+feature.properties.ANC_ID+'.org" target="_blank">About '+feature.properties.NAME+'</a></h4>');
     marker3.on('mouseover', function (event) {
   this.openPopup();
   layer = event.target
@@ -124,10 +158,64 @@ marker3.on('mouseout', function (event) {
         })
       }
   })
+  d3.json(neighborhoodsURL, function(neighborhoodData) {
+  var neighborhood = L.geoJSON(neighborhoodData, {
+  onEachFeature: function (feature, layer) {
+    var marker4 = layer.bindPopup('<h3>'+feature.properties.NBH_NAMES+'</h3>');
+    marker4.on('mouseover', function (event) {
+  this.openPopup();
+  layer = event.target
+  layer.setStyle({
+    fillOpacity: 0.8
+  })
+});
+marker4.on('mouseout', function (event) {
+  this.closePopup();
+  layer = event.target
+  layer.setStyle({
+    fillOpacity: 0.2
+  })
+});
+        marker4.on('click', function(event) {
+          myMap.fitBounds(event.target.getBounds());
+        })
+      }
+  })
+  d3.json(zipcodesURL, function(zipcodeData) {
+  var zipcode = L.geoJSON(zipcodeData, {
+        style: function(feature) {
+      return {
+        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        color: "green"
+      };
+    },
+  onEachFeature: function (feature, layer) {
+    var marker5 = layer.bindPopup('<h3>'+feature.properties.ZIPCODE+'</h3><h4>'+feature.properties.NAME+'</h4>');
+    marker5.on('mouseover', function (event) {
+  this.openPopup();
+  layer = event.target
+  layer.setStyle({
+    fillOpacity: 0.8
+  })
+});
+marker5.on('mouseout', function (event) {
+  this.closePopup();
+  layer = event.target
+  layer.setStyle({
+    fillOpacity: 0.2
+  })
+});
+        marker5.on('click', function(event) {
+          myMap.fitBounds(event.target.getBounds());
+        })
+      }
+  })
   var overlayMaps = {
     "DC Boundary": boundary,
     "DC Quadrants": quad,
     "DC Wards": ward,
+    "DC Zipcodes": zipcode, 
+    "DC Neighborhoods": neighborhood,
     "DC ANC": anc,
     "DC Single Member Districts": smd
   }
@@ -135,6 +223,8 @@ marker3.on('mouseout', function (event) {
 
 
   L.control.layers(baseMaps, overlayMaps, {collapsed:false}).addTo(myMap);
+})
+})
 })
 })
 })
