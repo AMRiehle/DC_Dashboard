8ed39343a43ada6f6a4eaadb6fb11591e2367ad1
Anna Riehle <riehlea@gmail.com>
2018-06-27 01:20:21 -0400

    Create heat map of crime data

--- auxiliary/dc-maps/js/crimes.js
+++ auxiliary/dc-maps/js/crimes.js
@@ -1,9 +1,15 @@
 boundaryURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/dc-boundary.geojson"
 quadrantURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/dc-quadrants.geojson"
-policeDistrictsURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/police-districts-mpd.geojson"
-gunshot_or_firecracker_URL = "https://raw.githubusercontent.com/AMRiehle/DC_Dashboard/master/auxiliary/dc-maps/local-datasets/gunshot-or-firecracker.json"
-single_gunshot_URL = "https://raw.githubusercontent.com/AMRiehle/DC_Dashboard/master/auxiliary/dc-maps/local-datasets/single-gunshots.json"
-multi_gunshot_URL = "https://raw.githubusercontent.com/AMRiehle/DC_Dashboard/master/auxiliary/dc-maps/local-datasets/multi-gunshots.json"
+neighborhoodsURL = "https://raw.githubusercontent.com/AMRiehle/DC_Dashboard/master/auxiliary/dc-maps/local-datasets/neighborhood-clusters.geojson"
+policeURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/police-districts-mpd.geojson"
+ancURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/advisory-neighborhood-commission-2013.geojson"
+smdURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/single-member-district-2013.geojson"
+zipcodesURL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/zip-codes.geojson"
+data2012URL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/crime-incidents-2012.geojson"
+data2013URL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/crime-incidents-2013.geojson"
+data2014URL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/crime-incidents-2014.geojson"
+data2015URL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/crime-incidents-2015.geojson"
+data2016URL = "https://raw.githubusercontent.com/benbalter/dc-maps/master/maps/crime-incidents-2016.geojson"
 
 var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
     "access_token=pk.eyJ1IjoicmllaGxlYSIsImEiOiJjamlhdWlzcnkxMndiM3FsbWl1aXE0MXJtIn0.g7oyFuzbGAh1O0SXpGI8nw");
@@ -11,21 +17,6 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v1
 var baseMaps = {
   "Street Map": streetmap,
 };
-
-function chooseColor(quadrant) {
-  switch (quadrant) {
-  case "SW":
-    return "yellow";
-  case "SE":
-    return "red";
-  case "NE":
-    return "orange";
-  case "NW":
-    return "green";
-  default:
-    return "black";
-  }
-}
 
 d3.json(boundaryURL, function(boundaryData) {
   var boundary = L.geoJSON(boundaryData, {
@@ -62,65 +53,384 @@ myMap.fitBounds(event.target.getBounds());
     style: function(feature) {
       return {
         // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-        fillColor: chooseColor(feature.properties.QUADRANT),
-        fillOpacity: 0.5,
-        weight: 1.5
+        fillColor: "black",
+        fillOpacity: 0,
+        weight: 3
       };
     }
 })
-   d3.json(policeDistrictsURL, function(policeData) {
-  var police = L.geoJSON(policeData, {
+
+  d3.json(neighborhoodsURL, function(neighborhoodData) {
+  var neighborhood = L.geoJSON(neighborhoodData, {
+        style: function(feature) {
+      return {
+        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        fillColor: "black",
+        fillOpacity: 0,
+        weight: 3
+      };
+    },
   onEachFeature: function (feature, layer) {
-    var marker5 = layer.bindPopup('<h3>Police District '+feature.properties.DISTRICT+'</h3>');
-    marker5.on('mouseover', function (event) {
+    var marker4 = layer.bindPopup('<h3>'+feature.properties.SHORT_NAME+'</h3>');
+    marker4.on('mouseover', function (event) {
   this.openPopup();
-  layer = event.target
-  layer.setStyle({
-    fillOpacity: 0.8
-  })
 });
-marker5.on('mouseout', function (event) {
+marker4.on('mouseout', function (event) {
   this.closePopup();
-  layer = event.target
-  layer.setStyle({
-    fillOpacity: 0.2
-  })
 });
-        marker5.on('click', function(event) {
+        marker4.on('click', function(event) {
           myMap.fitBounds(event.target.getBounds());
         })
       }
   })
 
-  d3.json(gunshot_or_firecracker_URL, function(response) {
-  var GunshotOrFirecracker = L.markerClusterGroup();
-  for (var i = 0; i < response.length; i++) {
-    GunshotOrFirecracker.addLayer(L.marker([response[i]['Lat (100m)'], response[i]['Lon (100m)']], {icon: firecrackerIcon})
-        .bindPopup('<h3>'+response[i].Date+'</h3><h3>'+response[i].Time+'</h3><h3>'+response[i].Type+'</h3>'));
+    d3.json(policeURL, function(policeData) {
+  var police = L.geoJSON(policeData, {
+        style: function(feature) {
+      return {
+        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        fillColor: "black",
+        fillOpacity: 0,
+        weight: 3
+      };
+    },
+  onEachFeature: function (feature, layer) {
+    var marker4 = layer.bindPopup('<h3>District '+feature.properties.DISTRICT+'</h3>');
+    marker4.on('mouseover', function (event) {
+  this.openPopup();
+});
+marker4.on('mouseout', function (event) {
+  this.closePopup();
+});
+        marker4.on('click', function(event) {
+          myMap.fitBounds(event.target.getBounds());
+        })
+      }
+  })
+
+    d3.json(zipcodesURL, function(zipcodesData) {
+  var zipcodes = L.geoJSON(zipcodesData, {
+        style: function(feature) {
+      return {
+        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        fillColor: "black",
+        fillOpacity: 0,
+        weight: 3
+      };
+    },
+  onEachFeature: function (feature, layer) {
+    var marker4 = layer.bindPopup('<h3>'+feature.properties.ZIPCODE+'</h3>');
+    marker4.on('mouseover', function (event) {
+  this.openPopup();
+});
+marker4.on('mouseout', function (event) {
+  this.closePopup();
+});
+        marker4.on('click', function(event) {
+          myMap.fitBounds(event.target.getBounds());
+        })
+      }
+  })
+
+    d3.json(ancURL, function(ancData) {
+  var anc = L.geoJSON(ancData, {
+        style: function(feature) {
+      return {
+        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        fillColor: "black",
+        fillOpacity: 0,
+        weight: 3
+      };
+    },
+  onEachFeature: function (feature, layer) {
+    var marker4 = layer.bindPopup('<h3>'+feature.properties.NAME+'</h3>');
+    marker4.on('mouseover', function (event) {
+  this.openPopup();
+});
+marker4.on('mouseout', function (event) {
+  this.closePopup();
+});
+        marker4.on('click', function(event) {
+          myMap.fitBounds(event.target.getBounds());
+        })
+      }
+  })
+
+    d3.json(smdURL, function(smdData) {
+  var smd = L.geoJSON(smdData, {
+        style: function(feature) {
+      return {
+        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        fillColor: "black",
+        fillOpacity: 0,
+        weight: 3
+      };
+    },
+  onEachFeature: function (feature, layer) {
+    var marker4 = layer.bindPopup('<h3>ANC '+feature.properties.SMD_ID+'</h3>');
+    marker4.on('mouseover', function (event) {
+  this.openPopup();
+});
+marker4.on('mouseout', function (event) {
+  this.closePopup();
+});
+        marker4.on('click', function(event) {
+          myMap.fitBounds(event.target.getBounds());
+        })
+      }
+  })
+
+d3.json(data2012URL, function(data2012response) {
+
+          theft = []
+          theft_from_auto = []
+          robbery = []
+          burglary = []
+          car_theft = []
+          assault = []
+          sex_abuse = []
+          homicide = []
+          arson = []
+
+  var data2012 = L.geoJSON(data2012response, {
+    onEachFeature: function (feature) {
+      if (feature.properties.OFFENSE == "ARSON") {
+        arson.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "THEFT F/AUTO") {
+        theft_from_auto.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "THEFT/OTHER") {
+        theft.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "ROBBERY") {
+        robbery.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "BURGLARY") {
+        burglary.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "MOTOR VEHICLE THEFT") {
+        car_theft.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "ASSAULT W/DANGEROUS WEAPON") {
+        assault.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "SEX ABUSE") {
+        sex_abuse.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else {
+        homicide.push(feature.geometry.coordinates)
+      }
   }
-  d3.json(single_gunshot_URL, function(response) {
-  var singleGunshots = L.markerClusterGroup();
-  for (var i = 0; i < response.length; i++) {
-    singleGunshots.addLayer(L.marker([response[i]['Lat (100m)'], response[i]['Lon (100m)']], {icon: singleGunIcon})
-        .bindPopup('<h3>'+response[i].Date+'</h3><h3>'+response[i].Time+'</h3><h3>'+response[i].Type+'</h3>'));
+})
+
+  d3.json(data2013URL, function(data2013response) {
+
+  var data2013 = L.geoJSON(data2013response, {
+    onEachFeature: function (feature) {
+      if (feature.properties.OFFENSE == "ARSON") {
+        arson.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "THEFT F/AUTO") {
+        theft_from_auto.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "THEFT/OTHER") {
+        theft.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "ROBBERY") {
+        robbery.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "BURGLARY") {
+        burglary.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "MOTOR VEHICLE THEFT") {
+        car_theft.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "ASSAULT W/DANGEROUS WEAPON") {
+        assault.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "SEX ABUSE") {
+        sex_abuse.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else {
+        homicide.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
   }
-  d3.json(multi_gunshot_URL, function(response) {
-  var multiGunshots = L.markerClusterGroup();
-  for (var i = 0; i < response.length; i++) {
-    multiGunshots.addLayer(L.marker([response[i]['Lat (100m)'], response[i]['Lon (100m)']], {icon: multiGunIcon})
-        .bindPopup('<h3>'+response[i].Date+'</h3><h3>'+response[i].Time+'</h3><h3>'+response[i].Type+'</h3>'));
-}  
- 
+})
+
+    d3.json(data2014URL, function(data2014response) {
+
+  var data2014 = L.geoJSON(data2014response, {
+    onEachFeature: function (feature) {
+      if (feature.properties.OFFENSE == "ARSON") {
+        arson.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "THEFT F/AUTO") {
+        theft_from_auto.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "THEFT/OTHER") {
+        theft.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "ROBBERY") {
+        robbery.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "BURGLARY") {
+        burglary.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "MOTOR VEHICLE THEFT") {
+        car_theft.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "ASSAULT W/DANGEROUS WEAPON") {
+        assault.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "SEX ABUSE") {
+        sex_abuse.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else {
+        homicide.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+  }
+})
+
+      d3.json(data2015URL, function(data2015response) {
+
+  var data2015 = L.geoJSON(data2015response, {
+    onEachFeature: function (feature) {
+      if (feature.properties.OFFENSE == "ARSON") {
+        arson.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "THEFT F/AUTO") {
+        theft_from_auto.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "THEFT/OTHER") {
+        theft.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "ROBBERY") {
+        robbery.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "BURGLARY") {
+        burglary.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "MOTOR VEHICLE THEFT") {
+        car_theft.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "ASSAULT W/DANGEROUS WEAPON") {
+        assault.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "SEX ABUSE") {
+        sex_abuse.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else {
+        homicide.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+  }
+})
+
+        d3.json(data2016URL, function(data2016response) {
+  var data2016 = L.geoJSON(data2016response, {
+    onEachFeature: function (feature) {
+      if (feature.properties.OFFENSE == "ARSON") {
+        arson.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "THEFT F/AUTO") {
+        theft_from_auto.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "THEFT/OTHER") {
+        theft.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "ROBBERY") {
+        robbery.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "BURGLARY") {
+        burglary.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "MOTOR VEHICLE THEFT") {
+        car_theft.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "ASSAULT W/DANGEROUS WEAPON") {
+        assault.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else if (feature.properties.OFFENSE == "SEX ABUSE") {
+        sex_abuse.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+      else {
+        homicide.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
+      }
+  }
+})
+
+  var arson_heat = L.heatLayer(arson, {
+    radius: 20,
+    blur: 35
+  })
+
+  var theft_from_auto_heat = L.heatLayer(theft_from_auto, {
+    radius: 20,
+    blur: 35
+  })  
+
+  var theft_heat = L.heatLayer(theft, {
+    radius: 20,
+    blur: 35
+  })  
+
+  var robbery_heat = L.heatLayer(robbery, {
+    radius: 20,
+    blur: 35
+  })  
+
+  var burglary_heat = L.heatLayer(burglary, {
+    radius: 20,
+    blur: 35
+  })  
+
+  var car_theft_heat = L.heatLayer(car_theft, {
+    radius: 20,
+    blur: 35
+  })  
+
+  var assault_heat = L.heatLayer(assault, {
+    radius: 20,
+    blur: 35
+  })  
+
+  var sex_abuse_heat = L.heatLayer(sex_abuse, {
+    radius: 20,
+    blur: 35
+  })  
+
+  var homicide_heat = L.heatLayer(homicide, {
+    radius: 20,
+    blur: 35
+  })  
+
   var overlayMaps = {
     "DC Boundary": boundary,
     "DC Quadrants": quad,
     "DC Police Districts": police,
-    "Gunshot or Firecracker Locations": GunshotOrFirecracker,
-    "Single Gunshot Locations": singleGunshots,
-    "Multiple Gunshot Locations": multiGunshots,
+    "DC Neighborhoods": neighborhood,
+    "DC Zip Codes": zipcodes,
+    "DC Advisory Neighborhood Commissions":anc,
+    "DC Single Member Districts": smd,
+    "Theft from Auto": theft_from_auto_heat,
+    "Theft - Other": theft_heat,
+    "Robbery": robbery_heat,
+    "Burglary": burglary_heat,
+    "Car Theft": car_theft_heat,
+    "Assault with a Dangerous Weapon": assault_heat,
+    "Sexual Abuse": sex_abuse_heat,
+    "Homicide": homicide_heat,
+    "Arson": arson_heat
   }
 
   L.control.layers(baseMaps, overlayMaps, {collapsed:false}).addTo(myMap);
+
+})
+})
+})
+})
+})
+})
 })
 })
 })
